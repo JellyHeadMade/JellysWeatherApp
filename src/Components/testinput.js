40632b4691dc4ffca6fe1437862e4eb1419c984c
JellyHead <JellyHeadM@outlook.com>
2022-12-28 18:32:01 -0500

    added in the call to the weather api and it is working. only issue is im failing to understand how to properly hid the api key. if i put it in a route on the backend, others can just see the backend route and get the key anyway, and if i put it in an env file on the front end, its complied and can be seen. for now i do not have the route to my backend actually filled in to the api key, and will look into what i can do to the route to prevent anyone from getting the key themselves.

--- src/Components/testinput.js
+++ src/Components/testinput.js
@@ -1,6 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+var apiKey = '';
 
 function TestInput() {
+
+    useEffect(() => {
+        axios.get('https://scjournalapiv2.herokuapp.com/')
+        .then(function (res) {
+            apiKey = res.data.key;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }, []);
 
     const [city, setCity] = useState('');
 
@@ -12,6 +25,16 @@ function TestInput() {
         console.log('submitting'); 
         e.preventDefault();
         console.log(city);
+        console.log(apiKey);
+        if (city.length > 10) {
+            axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7`)
+            .then(function (response) {
+            console.log(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
     }
 
   return (
