50826a15684dfd36d6088ee584adbff20bbf587b
JellyHead <JellyHeadM@outlook.com>
2023-01-14 23:36:57 -0500

    simply added a line to readout the avg temp coming from the api. i orginally kept trying to return the entire stack but i will have to go through every data point that matters to me and display them individually as i get an error doing it any other way. a map function is definatly what i would like to use.

--- src/Components/SubComponents/weatherresults.js
+++ src/Components/SubComponents/weatherresults.js
@@ -3,7 +3,8 @@ import React from 'react';
 function WeatherResults(props) {
     return (
         <div>
-        <h1>Weather Results {console.log(props)}</h1>
+        <h1>Weather Results {console.log(props.data[0])}</h1>
+        <div className="weather-results">current temp is {props.data[0].day.avgtemp_f}F</div>
         </div>
     );
 }
