abf28c05254418ac45764f939c00b22954daa7e6
JellyHead <JellyHeadM@outlook.com>
2023-01-16 00:19:51 -0500

    added the abitlity to select the metric you would want to use for the weather, between F and C

--- src/Components/SubComponents/weatherresults.js
+++ src/Components/SubComponents/weatherresults.js
@@ -9,11 +9,11 @@ function WeatherResults(props) {
             return (
                 <div>
                     <div key={ForcastDay.date}>{ForcastDay.date}</div>
-                    <div key={ForcastDay.date}>Average Tempature for the day is {ForcastDay.day.avgtemp_f}</div>
+                    <div key={ForcastDay.date}>Average Tempature for the day is {props.metric === 'f' ? ForcastDay.day.avgtemp_f : ForcastDay.day.avgtemp_c}</div>
                     <div key={ForcastDay.date}>The Condition for the day is {ForcastDay.day.condition.text}</div>
                     <div key={ForcastDay.date}>Chance of rain is {ForcastDay.day.daily_will_it_rain}</div>
-                    <div key={ForcastDay.date}>High tempature for the day is {ForcastDay.day.maxtemp_f}</div>
-                    <div key={ForcastDay.date}>Low tempature for the day is {ForcastDay.day.mintemp_f}</div>
+                    <div key={ForcastDay.date}>High tempature for the day is {props.metric === 'f' ? ForcastDay.day.maxtemp_f : ForcastDay.day.maxtemp_c}</div>
+                    <div key={ForcastDay.date}>Low tempature for the day is {props.metric === 'f' ? ForcastDay.day.mintemp_f : ForcastDay.day.mintemp_c}</div>
                     <div key={ForcastDay.date}>Humidity for the day : {ForcastDay.day.avghumidity}</div>
                 </div>
             )
