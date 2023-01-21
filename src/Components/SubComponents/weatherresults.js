686be79be8872ec3486676ce37ed97715943e9a7
JellyHead <JellyHeadM@outlook.com>
2023-01-20 19:19:37 -0500

    added a bg div so i could separate each of the cards with color. also started changing each of the results styles as well.

--- src/Components/SubComponents/weatherresults.js
+++ src/Components/SubComponents/weatherresults.js
@@ -5,16 +5,18 @@ function WeatherResults(props) {
         <div>
         <h1>Weather Results</h1>
         <div className="current-temp">current temp is {props.metric == 'f' ? props.data[0].day.avgtemp_f : props.data[0].day.avgtemp_c}F</div>
-        <div className='weather-car-wrapper'>{props.data.map((ForcastDay, index) => {
+        <div className='weather-card-wrapper'>{props.data.map((ForcastDay, index) => {
             return (
                 <div className='weather-report-day-wrapper'>
-                    <div className='weather-report-date' key={index}>{ForcastDay.date}</div>
-                    <div className='weather-report-avg-temp' key={index}>Average Tempature for the day is {props.metric === 'f' ? ForcastDay.day.avgtemp_f : ForcastDay.day.avgtemp_c}</div>
-                    <div className='weather-report-condition' key={index}>The Condition for the day is {ForcastDay.day.condition.text}</div>
-                    <div className='weather-report-rain' key={index}>Chance of rain is {ForcastDay.day.daily_will_it_rain}</div>
-                    <div className='weather-report-high-temp' key={index}>High tempature for the day is {props.metric === 'f' ? ForcastDay.day.maxtemp_f : ForcastDay.day.maxtemp_c}</div>
-                    <div className='weather-report-low-temp' key={index}>Low tempature for the day is {props.metric === 'f' ? ForcastDay.day.mintemp_f : ForcastDay.day.mintemp_c}</div>
-                    <div className='weather-report-humidity' key={index}>Humidity for the day : {ForcastDay.day.avghumidity}</div>
+                    <div className='weather-report-bg'>
+                        <div className='weather-report-text__date' key={index}>{ForcastDay.date}</div>
+                        <div className='weather-report-text__avg-temp' key={index}>Average Tempature for the day is {props.metric === 'f' ? ForcastDay.day.avgtemp_f : ForcastDay.day.avgtemp_c}</div>
+                        <div className='weather-report-text__condition' key={index}>The Condition for the day is {ForcastDay.day.condition.text}</div>
+                        <div className='weather-report-text__rain' key={index}>Chance of rain is {ForcastDay.day.daily_will_it_rain}</div>
+                        <div className='weather-report-text__high-temp' key={index}>High tempature for the day is {props.metric === 'f' ? ForcastDay.day.maxtemp_f : ForcastDay.day.maxtemp_c}</div>
+                        <div className='weather-report-text__low-temp' key={index}>Low tempature for the day is {props.metric === 'f' ? ForcastDay.day.mintemp_f : ForcastDay.day.mintemp_c}</div>
+                        <div className='weather-report-text__humidity' key={index}>Humidity for the day : {ForcastDay.day.avghumidity}</div>
+                    </div>
                 </div>
             )
         })}</div>
