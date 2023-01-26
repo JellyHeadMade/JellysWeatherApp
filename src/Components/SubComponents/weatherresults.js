8a758560d13ca513574a56dde8b439882c004c7a
JellyHead <JellyHeadM@outlook.com>
2023-01-25 21:11:54 -0500

    added a flip state variable, as well as a function called flipped to manage on onclick. at the moment it does what it should, unfornatly there seems to be some structure issues that i will need to work on when transistioning from one to the other. as when its flipped i loose all styles. will work on this tomorrow, but i think this is still great progess as i have styles for both sides of the cards, the tranistion is one of the larger issues. another issues i will have to resolve is when i click one card, all cards flip when i will only want the card that is click to flip. this also goes back to the structure issue i am having.

--- src/Components/SubComponents/weatherresults.js
+++ src/Components/SubComponents/weatherresults.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function WeatherResults(props) {
+
+    const [flip, setFlip] = useState(false);
+
+    const flipped = () => {
+        console.log('trying to flip');
+        if (!flip) {  
+            setFlip(true);
+        } else {
+            setFlip(false);
+        }
+    }
     return (
         <div>
         <h1>Weather Results</h1>
         <div className="current-temp">current temp is {props.metric === 'f' ? props.data[0].day.avgtemp_f : props.data[0].day.avgtemp_c}F</div>
         <div className='weather-card-wrapper'>{props.data.map((ForcastDay, index) => {
             return (
-                <div className='weatherresult-card'>
-                    <div className='card-bg'>
-                        <div className='weather-card-text__date' key={index}>{ForcastDay.date}</div>
-                        <div className='weather-card-text' key={index}>Average Tempature for the day is {props.metric === 'f' ? ForcastDay.day.avgtemp_f : ForcastDay.day.avgtemp_c}</div>
-                        <div className='weather-card-text' key={index}>The Condition for the day is {ForcastDay.day.condition.text}</div>
-                        <div className='weather-card-text' key={index}>Chance of rain is {ForcastDay.day.daily_will_it_rain}</div>
-                        <div className='weather-card-text' key={index}>High tempature for the day is {props.metric === 'f' ? ForcastDay.day.maxtemp_f : ForcastDay.day.maxtemp_c}</div>
-                        <div className='weather-card-text' key={index}>Low tempature for the day is {props.metric === 'f' ? ForcastDay.day.mintemp_f : ForcastDay.day.mintemp_c}</div>
-                        <div className='weather-card-text' key={index}>Humidity for the day : {ForcastDay.day.avghumidity}</div>
+                <div className='weather-card'>
+                    <div onClick={flipped} className={flip ? 'weather-card-inner.isflipped' : 'weather-card-inner'}>
+                        <div className='card__face weather-card-front'>{ForcastDay.date}</div>
+                        <div className='card__face weather-card-back'>
+                            <div className='weather-card-text__date' key={index}>{ForcastDay.date}</div>
+                            <div className='weather-card-text' key={index}>Average Tempature for the day is {props.metric === 'f' ? ForcastDay.day.avgtemp_f : ForcastDay.day.avgtemp_c}</div>
+                            <div className='weather-card-text' key={index}>The Condition for the day is {ForcastDay.day.condition.text}</div>
+                            <div className='weather-card-text' key={index}>Chance of rain is {ForcastDay.day.daily_will_it_rain}</div>
+                            <div className='weather-card-text' key={index}>High tempature for the day is {props.metric === 'f' ? ForcastDay.day.maxtemp_f : ForcastDay.day.maxtemp_c}</div>
+                            <div className='weather-card-text' key={index}>Low tempature for the day is {props.metric === 'f' ? ForcastDay.day.mintemp_f : ForcastDay.day.mintemp_c}</div>
+                            <div className='weather-card-text' key={index}>Humidity for the day : {ForcastDay.day.avghumidity}</div>
+                        </div>
                     </div>
                 </div>
             )
