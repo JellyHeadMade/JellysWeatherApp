8c0840289fd070a252fed36e5110325a2ad61801
JellyHead <JellyHeadM@outlook.com>
2023-01-27 23:45:49 -0500

    updated the structure to dynamically use the index number with a new state variable object to keep track of the flip bool for each index. also updated the onclick function to handle it correctly. unfortunatly i still cannot get a smooth flip, or really have the backside have any of its styles. will have to keep messing around. everything seems like it should work but i am clearly missing something right in my face.

--- src/Components/SubComponents/weatherresults.js
+++ src/Components/SubComponents/weatherresults.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 
 function WeatherResults(props) {
 
-    const [flip, setFlip] = useState(false);
+    const [flip, setFlip] = useState({
+        0: false,
+        1: false,
+        2: false,
+    });
 
     const flipped = (index) => {
         console.log('trying to flip' + index);
-        if (!flip) {  
-            setFlip(true);
+        if (flip[index] === false) {
+            setFlip({ ...flip, [index]: true });
         } else {
-            setFlip(false);
+            setFlip({ ...flip, [index]: false });
         }
     }
     return (
@@ -18,18 +22,16 @@ function WeatherResults(props) {
         <div className="current-temp">current temp is {props.metric === 'f' ? props.data[0].day.avgtemp_f : props.data[0].day.avgtemp_c}F</div>
         <div className='weather-card-wrapper'>{props.data.map((ForcastDay, index) => {
             return (
-                <div className='weather-card'>
-                    <div onClick={() => flipped(index)} className={flip ? 'weather-card-inner.isflipped' : 'weather-card-inner'}>
-                        <div className='card__face weather-card-front'>{ForcastDay.date}</div>
-                        <div className='card__face weather-card-back'>
-                            <div className='weather-card-text__date' key={index}>{ForcastDay.date}</div>
-                            <div className='weather-card-text' key={index}>Average Tempature for the day is {props.metric === 'f' ? ForcastDay.day.avgtemp_f : ForcastDay.day.avgtemp_c}</div>
-                            <div className='weather-card-text' key={index}>The Condition for the day is {ForcastDay.day.condition.text}</div>
-                            <div className='weather-card-text' key={index}>Chance of rain is {ForcastDay.day.daily_will_it_rain}</div>
-                            <div className='weather-card-text' key={index}>High tempature for the day is {props.metric === 'f' ? ForcastDay.day.maxtemp_f : ForcastDay.day.maxtemp_c}</div>
-                            <div className='weather-card-text' key={index}>Low tempature for the day is {props.metric === 'f' ? ForcastDay.day.mintemp_f : ForcastDay.day.mintemp_c}</div>
-                            <div className='weather-card-text' key={index}>Humidity for the day : {ForcastDay.day.avghumidity}</div>
-                        </div>
+                <div onClick={() => flipped(index)} className={flip[index] ? `weather-card-inner-${index}.isflipped` : `weather-card-inner-${index}`}>
+                    <div className='card__face weather-card-front'>{ForcastDay.date}</div>
+                    <div className='card__face weather-card-back'>
+                        <div className='weather-card-text__date' key={index}>{ForcastDay.date}</div>
+                        <div className='weather-card-text' key={index}>Average Tempature for the day is {props.metric === 'f' ? ForcastDay.day.avgtemp_f : ForcastDay.day.avgtemp_c}</div>
+                        <div className='weather-card-text' key={index}>The Condition for the day is {ForcastDay.day.condition.text}</div>
+                        <div className='weather-card-text' key={index}>Chance of rain is {ForcastDay.day.daily_will_it_rain}</div>
+                        <div className='weather-card-text' key={index}>High tempature for the day is {props.metric === 'f' ? ForcastDay.day.maxtemp_f : ForcastDay.day.maxtemp_c}</div>
+                        <div className='weather-card-text' key={index}>Low tempature for the day is {props.metric === 'f' ? ForcastDay.day.mintemp_f : ForcastDay.day.mintemp_c}</div>
+                        <div className='weather-card-text' key={index}>Humidity for the day : {ForcastDay.day.avghumidity}</div>
                     </div>
                 </div>
             )
