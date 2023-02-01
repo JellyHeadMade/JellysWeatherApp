cbacf22dcf643395c4fec0c4d19931beb1ffca80
JellyHead <JellyHeadM@outlook.com>
2023-02-01 17:32:36 -0500

    moved all the day count buttons, and both metric buttons into a wrapper div so i could use grid and lay them out, also implemented two new state variables for each type of buttons that will set them as active when clicked while setting all others to none active. even the cursor is set to defualt when its active to indicate its no longer clickable as the onclick is tied to the button being active. this should be a good simplification over my previous try at this on the scjournal. still some optimizations that i could make but its working at the moment.

--- src/Components/SubComponents/areaselector.js
+++ src/Components/SubComponents/areaselector.js
@@ -5,11 +5,58 @@ import WeatherResults from './weatherresults';
 function AreaSelector() {
 
     const [area, setArea] = useState('');
-    const [daycount, setDayCount] = useState(0);
+    const [daycount, setDayCount] = useState(1);
     const [metric, setMetric] = useState('f');
     const [weatherdata, setWeatherData] = useState({
         forecast: null
     });
+    const [dayButtonActive, setDayButtonActive] = useState({
+        1: true,
+        2: false,
+        3: false
+    });
+    const [metricButtonActive, setMetricButtonActive] = useState({
+        f: true,
+        c: false
+    });
+
+    const dayButtonOnClick = (value) => {
+        console.log(value);
+        switch (value) {
+            case 1:
+                setDayButtonActive({ 1: true, 2: false, 3: false });
+                setDayCount(1);
+                break;
+            case 2:
+                setDayButtonActive({ 1: false, 2: true, 3: false });
+                setDayCount(2);
+                break;
+            case 3:
+                setDayButtonActive({ 1: false, 2: false, 3: true });
+                setDayCount(3);
+                break;
+            default:
+                setDayButtonActive({ 1: true, 2: false, 3: false });
+                setDayCount(1);
+        }
+    }
+
+    const metricButtonOnClick = (value) => {
+        console.log(value);
+        switch (value) {
+            case 'f':
+                setMetricButtonActive({ f: true, c: false });
+                setMetric('f');
+                break;
+            case 'c':
+                setMetricButtonActive({ f: false, c: true });
+                setMetric('c');
+                break;
+            default:
+                setMetricButtonActive({ f: true, c: false });
+                setMetric('f');
+        }
+    }
 
     const handleChange = (e) => {
         setArea(e.target.value);
@@ -44,11 +91,18 @@ function AreaSelector() {
             <h2 className='input-message'>Please input your area code to get the Weather!</h2>
             <label className='area-label'>Area</label>
             <input className='input-field' type="text" value={area} onChange={handleChange} />
-            <div className='day-count-btn-1' onClick={() => setDayCount(1)}>Current</div>
-            <div className='day-count-btn-2' onClick={() => setDayCount(2)}>2 Days</div>
-            <div className='day-count-btn-3' onClick={() => setDayCount(3)}>3 Days</div>
-            <div className='metric-count-btn-f' onClick={() => setMetric('f')}>Fehrinheight</div>
-            <div className='metric-count-btn-c' onClick={() => setMetric('c')}>celcius</div>
+            <div className='button-selector-wrapper'>
+                {dayButtonActive[1] ? <div className='day-count-btn-1' style={{backgroundColor: '#00FF00', transition: '0.2s ease-in-out', cursor: 'default'}}>Current</div> :
+                    <div className='day-count-btn-1' onClick={() => dayButtonOnClick(1)} style={{backgroundColor: '#C0C0C0', transition: '0.2s ease-in-out'}}>Current</div>}
+                {dayButtonActive[2] ? <div className='day-count-btn-2' style={{backgroundColor: '#00FF00', transition: '0.2s ease-in-out', cursor: 'default'}}>2 Days</div> :
+                    <div className='day-count-btn-2' onClick={() => dayButtonOnClick(2)} style={{backgroundColor: '#C0C0C0', transition: '0.2s ease-in-out'}}>2 Days</div>}
+                {dayButtonActive[3] ? <div className='day-count-btn-3' style={{backgroundColor: '#00FF00', transition: '0.2s ease-in-out', cursor: 'default'}}>3 Days</div> :
+                    <div className='day-count-btn-3' onClick={() => dayButtonOnClick(3)} style={{backgroundColor: '#C0C0C0', transition: '0.2s ease-in-out'}}>3 Days</div>}
+                {metricButtonActive['f'] ? <div className='metric-count-btn-f' style={{backgroundColor: '#00FF00', transition: '0.2s ease-in-out', cursor: 'default'}}>fahrenheit</div> :
+                    <div className='metric-count-btn-f' onClick={() => metricButtonOnClick('f')} style={{backgroundColor: '#C0C0C0', transition: '0.2s ease-in-out'}}>fahrenheit</div>}
+                {metricButtonActive['c'] ? <div className='metric-count-btn-c' style={{backgroundColor: '#00FF00', transition: '0.2s ease-in-out', cursor: 'default'}}>celsius</div> :
+                    <div className='metric-count-btn-c' onClick={() => metricButtonOnClick('c')} style={{backgroundColor: '#C0C0C0', transition: '0.2s ease-in-out'}}>celsius</div>}
+            </div>
             <button className='submit-btn' type="submit">Submit</button>
             {weatherdata.forecast ? <WeatherResults metric={metric} data={weatherdata.forecast} /> : <h2 className='result-placeholder'>Please input an area code and click submit for Results!</h2>}
         </form>
